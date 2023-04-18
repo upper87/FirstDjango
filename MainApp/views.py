0071f0c30f6464e8b03b41e3fe85afb0ad3a8037
Stanislav <upper87@mail.ru>
2023-04-18 17:16:03 +0300

    1

--- MainApp/views.py
+++ MainApp/views.py
@@ -3,9 +3,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    text = """<h1>"Изучаем Django"</h1>
-          <strong> Автор </strong> : <i> Лобанов С.А.</i>"""
-    return HttpResponse(text)
+    context = {
+        "name": "Nikolay",
+        "email": "me_mail@mail.ru"
+    }
+    crumbs = breadcrumbs
+    # text = """<h1>"Изучаем Django"</h1>
+    #       <strong> Автор </strong> : <i> Лобанов С.А.</i>"""
+    # return HttpResponse(text)
+    return render(request, "index.html", context)
 
 about_dict = {
     "name": "Иван",
@@ -26,27 +32,40 @@ def about(request):
     return HttpResponse(result)
 
 items = [
-   {"id": 1, "name": "Кроссовки abibas"},
-   {"id": 2, "name": "Куртка кожаная"},
-   {"id": 3, "name": "Coca-cola 1 литр"},
-   {"id": 4, "name": "Картофель фри"},
-   {"id": 5, "name": "Кепка"},
+   {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
+   {"id": 2, "name": "Куртка кожаная" ,"quantity":2},
+   {"id": 3, "name": "Coca-cola 1 литр" ,"quantity":12},
+   {"id": 4, "name": "Картофель фри" ,"quantity":0},
+   {"id": 5, "name": "Кепка" ,"quantity":124},
+]
+
+breadcrumbs = [
+    {"page": "Главная" ,"link":"/"}
 ]
 
 def get_item(request, id):
     for item in items:
         if item["id"] == id:
-            res = f"""
-            <h1> Имя:{item["name"]} </h1>
-            """
-            return HttpResponse(res)
+            # res = f"""
+            # <h1> Имя:{item["name"]} </h1>
+            # <a href='/items'> Назад </a>
+            # """
+            # return HttpResponse(res)
+            context = {
+                "item": item
+            }
+            return render(request, "item.html", context)
     # print(f"{id=}, {type(id) = }")
     return HttpResponseNotFound("""<h1>Товара нету</h1>""")
 
 
 def get_items(request):
-    res_items = "<h2>Список товаров</h2><ol>"
-    for item in items:
-        res_items += f"""<li><a href="item/{item["id"]}">{item['name']}</a></li>"""
-    res_items += "</ol>"
-    return HttpResponse(res_items)
+    # res_items = "<h2>Список товаров</h2><ol>"
+    # for item in items:
+    #     res_items += f"""<li><a href="item/{item["id"]}">{item['name']}</a></li>"""
+    # res_items += "</ol>"
+    # return HttpResponse(res_items)
+    context = {
+        "items": items
+    }
+    return render(request, "items-list.html", context)
