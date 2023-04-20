97905d9fc1b7dd5f63d89b4abb660f711c5b1666
Stanislav <upper87@mail.ru>
2023-04-20 14:10:38 +0300

    day3_4

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from MainApp import views
 
 urlpatterns = [
-    path('', views.home),
-    path('about', views.about),
-    path('item/<int:id>', views.get_item),
-    path('items', views.get_items)
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('item/<int:id>', views.get_item, name='item-detail'),
+    path('items', views.get_items, name='items-list')
 ]
 
